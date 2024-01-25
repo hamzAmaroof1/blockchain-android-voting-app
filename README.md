1a715b8736c76addf445e9e35b074e233b04140f
hamza <hamzamaroof256@gmailcom>
2024-01-25 20:45:59 +0500

    miner changes done:

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # blockchain-android-voting-app
+# nothing-yet-dded 
+# owner Hamza 
+
+will be available soon keep with us 
--- contracts/hamza.sol
+++ contracts/hamza.sol
@@ -2,6 +2,17 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract hamza {
-  constructor() public {
-  }
+    struct Voter {
+        uint weight; // weight is accumulated by delegation
+        bool voted; // if true, that person already voted
+        address delegate; // person delegated to
+        uint vote; // index of the voted proposal
+    }
+  struct Proposal {
+        bytes32 name;   // short name (up to 32 bytes)
+        uint voteCount; // number of accumulated votes
+    }
+
+    address public chairperson;
+    constructor() public {}
 }
